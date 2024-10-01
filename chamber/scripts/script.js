3f65f0efa5ced1deaede3d8dd60dead3e80d6ca0
Valery Statov <vsgz2432@gmail.com>
2024-10-01 13:08:32 +0300

    added main chamber page

---
+++ chamber/scripts/script.js
@@ -0,0 +1,35 @@
+// Slider
+let currentIndex = 0;
+const slides = document.querySelectorAll('.slide');
+const slideInterval = 3000;
+ 
+function changeSlide() {
+  slides[currentIndex].style.opacity = 0;
+  currentIndex = (currentIndex + 1) % slides.length;
+  slides[currentIndex].style.opacity = 1;
+}
+ 
+setInterval(changeSlide, slideInterval);
+
+
+// Last modified date
+const modified = document.querySelector('#modified');
+
+modified.textContent = document.lastModified;
+
+
+// Burger button
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+const socials = document.querySelector('.socials');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+
+  if (socials.style.display !== 'none'){
+  	socials.style.display="none";
+  }else{
+  	socials.style.display="flex";
+  }
+});
